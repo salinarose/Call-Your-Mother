7bb53b4d9ccd67c567a1b877ef38518c869a983c
Salina <salinasharudin@gmail.com>
2023-12-04 16:58:22 -0500

    gridpane now populates with the contacts list

--- src/main/java/com/salinasharudin/TimezoneManager/MainController.java
+++ src/main/java/com/salinasharudin/TimezoneManager/MainController.java
@@ -5,15 +5,17 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class MainController implements Initializable {
 
-	//Create local clock
+	// Creates a local clock
 	private ZonedDateTime clockNow = ZonedDateTime.now();
 	private DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("hh:mm a");
 	private DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd MMM yyyy");
@@ -29,6 +31,7 @@ public class MainController implements Initializable {
 	@FXML
 	Label lblLocalDate;
 	
+	/* Displays a local clock, date, location, and time zone */
 	public void displayLocalDateTime() {
 		lblLocalLocation.setText(clockNow.getZone().toString());
 		lblLocalTimeZone.setText(clockNow.format(DateTimeFormatter.ofPattern("z")));
@@ -36,11 +39,31 @@ public class MainController implements Initializable {
 		lblLocalDay.setText(clockNow.getDayOfWeek().toString());
 		lblLocalDate.setText(clockNow.format(formatDate));
 	}
+	
+	@FXML
+	GridPane gridPaneContacts;
+	
+	/* Displays the user's list of contacts */
+	public void displayContacts() {
+		// Get the contact list
+		ArrayList<Contact> list = FileHelper.getContacts();
+		
+		// Clear the grid pane
+		gridPaneContacts.getChildren().clear();
+		
+		// Populate the grid pane with the new contacts
+		int i = 0;
+		for (Contact c : list) {
+			gridPaneContacts.addRow(i, new Label(c.getName()), new Label(c.getTimezone()));
+			i++;
+		}
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		/* Calls display methods */
 		displayLocalDateTime();
+		displayContacts();
 	}
     
 }
