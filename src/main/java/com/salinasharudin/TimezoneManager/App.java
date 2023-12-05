bcc0a69e40bc3d4251822e7e401b88a07b8bf78a
Salina <salinasharudin@gmail.com>
2023-12-04 21:00:07 -0500

    changed root scene size

--- src/main/java/com/salinasharudin/TimezoneManager/App.java
+++ src/main/java/com/salinasharudin/TimezoneManager/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
    
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main"), 640, 480);
+        scene = new Scene(loadFXML("Main"), 640, 640);
         stage.setScene(scene);
         stage.show();
     }
