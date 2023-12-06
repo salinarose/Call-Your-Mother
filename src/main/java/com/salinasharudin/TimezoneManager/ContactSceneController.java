6b703b1359b90a72f226fdc7e28e51ca4a5ce177
Salina <salinasharudin@gmail.com>
2023-12-06 02:35:42 -0500

    changed tableview to gridpane and set it up
    right now only shows the default view when adding a new contact (does
    not show data for editing existing contacts)

--- src/main/java/com/salinasharudin/TimezoneManager/ContactSceneController.java
+++ src/main/java/com/salinasharudin/TimezoneManager/ContactSceneController.java
@@ -3,14 +3,40 @@ package com.salinasharudin.TimezoneManager;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 
 public class ContactSceneController implements Initializable {
-
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		setupGrid();
 	}
-
+	
+	@FXML
+	GridPane gridAvailability;
+	
+	public void setupGrid() {
+		
+		// Hours column
+		for (int i = 1; i < 25; i++) {
+			//gridAvailability.addRow(i, new Label((i) + ":00"));
+			gridAvailability.add(new Label((i) + ":00"), 0, i);
+			for (int j = 1; j < 8; j++) {
+				gridAvailability.add(new CheckBox(), j, i);
+			}
+		}
+	}
+	
 }
