b3c4211091ae131cf8b03829e6671c861f2f3fdf
Salina <salinasharudin@gmail.com>
2023-12-18 00:35:08 -0500

    finished writing scheduleHelper
    still needs more robust testing

--- src/main/java/com/salinasharudin/TimezoneManager/ScheduleHelper.java
+++ src/main/java/com/salinasharudin/TimezoneManager/ScheduleHelper.java
@@ -11,12 +11,11 @@ import javafx.scene.control.Alert;
 
 public final class ScheduleHelper {
 	// Map will store all the hours that all members have mutually available for each day
-	public	Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+	public static Map<Integer, ArrayList<Integer>> map = new HashMap<>();
 	
 	// TODO: get members 
-	public void getMembers(ArrayList<Contact> others) {
+	public static void getMembers(ArrayList<Contact> others) {
 		Boolean[][] user = Settings.getInstance().getSchedule();
-
 		
 		// Get the user offset
 		ZoneOffset userOffset = OffsetDateTime.now(Settings.getInstance().getZone()).getOffset();
@@ -31,9 +30,13 @@ public final class ScheduleHelper {
 			return;
 		}
 		
-		for (Contact c : others) {
-			Boolean[][] other = c.getAvailability();
+		// get the differences for each other contact
+		int[] difs = new int[others.size()];
+		
+		for (int i = 0; i < others.size(); i++) {
+			//Boolean[][] other = c.getAvailability();
 			// Get the user's offset as an int
+			Contact c = others.get(i);
 			int otherOffsetInt = getOffsetInt(OffsetDateTime.now(ZoneId.of(c.getTimezone())).getOffset());
 			if (otherOffsetInt == -100) {
 				map.clear();
@@ -41,20 +44,68 @@ public final class ScheduleHelper {
 				return;
 			}
 			
-			int dif = getDif(userOffsetInt, otherOffsetInt);
-			
-			
-			//TODO: stopped here!!
-			//checkSchedule(dif);
-			
-			//map = getMutual(map, other, dif);
+			difs[i] = getDif(userOffsetInt, otherOffsetInt);
 		}
+		
+		/* Build the schedule */
+		for (int day = 0; day < 7; day++) {
+			for (int hr = 0; hr < 24; hr++) {
+				// Check if the user has that time available
+				if (user[day][hr] == true) {
+					
+					Boolean same = false;
+					// Check each contact's schedule
+					for (int i = 0; i < others.size(); i++) {
+						int dif = difs[i];
+						if (dif == 0) {
+							// same zone - no conversion needed
+							same = checkTime(others.get(i), day, hr);
+						} else {
+							// conversion needed
+							int new_day = (day + ((hr + dif) / 24)) % 7;
+							int new_hr = (hr + dif) % 24;
+							if (new_hr < 0) {
+								new_hr += 24;
+								if (new_day - 1 < 0) {
+									same = checkTime(others.get(i), 6, new_hr);
+								} else {
+								same = checkTime(others.get(i), new_day - 1, new_hr);
+								}
+							} else {
+								same = checkTime(others.get(i), new_day, new_hr);
+							}
+						}
+						// If this contact is compatible, no need to check the others
+						if (same != true) {
+							break;
+						}
+					}
+					
+					// If same is true, all of the contacts were available at that time
+					//map.put(day, map.get(day).add(hr));
+					if (same == true) {
+						System.out.println(day + ", " + hr);
+						
+				        if (!map.containsKey(day)) {
+				            map.put(day, new ArrayList<>());
+				        }
+				        map.get(day).add(hr);
+					}
+					
+				} else continue;
+			}
+		}
+		
 	}
 	
-
+	public static Boolean checkTime(Contact other, int day, int hr) {
+		if (other.getAvailability()[day][hr] != null &&
+				other.getAvailability()[day][hr] == true) return true;
+		else return false;
+	}
 	
 	// TODO: get offset
-	public int getOffsetInt(ZoneOffset offset) {
+	public static int getOffsetInt(ZoneOffset offset) {
 		String o = offset.toString();
 		o = o.substring(0, 3);
 		try {
@@ -68,19 +119,18 @@ public final class ScheduleHelper {
 	}
 	
 	// Gets the difference in offset between the user and the other contact. For example, -05 (user) and +08 (other) have a +13 difference.
-	public int getDif(int user, int other) {
+	public static int getDif(int user, int other) {
 		return (0 - user) - (0 - other);
 	}
-	
-	// TODO: check time 
+
 
 	// TODO: get mutual schedule
-	public void getMutual(Map<Integer, ArrayList<Integer>> map, Boolean[][] other) {
-
+	public Map<Integer, ArrayList<Integer>> getMutual() {
+		return this.map;
 	}
 	
 	/* Alert that shows if there is an error converting offset to int */
-	private void showErrorAlert() {
+	private static void showErrorAlert() {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setContentText("There was an error creating the schedule.");
 		alert.show();
